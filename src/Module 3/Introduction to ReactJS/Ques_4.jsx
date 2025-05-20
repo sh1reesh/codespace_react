9b65e3d8c74f5d465a9f936a4d16b0520cf2bf4f
Asrithya Vardhan <137252219+Asrithya@users.noreply.github.com>
2025-05-20 02:16:52 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -1,17 +1,19 @@
 // JSX Rules
 // Description: Identify and correct errors in incorrect JSX syntax.
 
+import React from 'react';
+
 function Error() {
-    return (
-        <div>
-            <h1>Unclosed tag
-            <p>Self-closing tag example <img src="example.png"></img></p>
-            <ul>
-                <li>List item 1
-                <li>List item 2
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Unclosed tag</h1>
+      <p>Self-closing tag example <img src="example.png" alt="Example" /></p>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+      </ul>
+    </div>
+  );
 }
 
-
+export default Error;
