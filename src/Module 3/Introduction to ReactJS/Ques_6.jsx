3a182078ac2d84cf872b2d9acdd1aa68e65a11eb
Asrithya Vardhan <137252219+Asrithya@users.noreply.github.com>
2025-05-20 02:19:38 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_6.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_6.jsx
@@ -4,3 +4,14 @@
 // Step 1: Write your code within the file, by the name of component as LoginMessage
 // Step 2: Modify the App.js to use LoginMessage with a boolean prop
 
+import React from 'react';
+
+function LoginMessage({ isLoggedIn }) {
+  return (
+    <div>
+      {isLoggedIn ? <h2>Welcome back!</h2> : <h2>Please log in.</h2>}
+    </div>
+  );
+}
+
+export default LoginMessage;
