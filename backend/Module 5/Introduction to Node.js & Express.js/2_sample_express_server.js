20a683c3b0ff4c127a28c07eec73d31458fc47c8
Asrithya Vardhan <137252219+Asrithya@users.noreply.github.com>
2025-05-20 05:48:48 +0000

    completed

--- backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
+++ backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
@@ -1,2 +1,10 @@
-// Creating a Simple Express Server
-// Description: Write a simple Express server that listens on port 3000.
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
