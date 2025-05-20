1e92732ccef44a46b22a1082e86ed4e400f4f0d2
Asrithya Vardhan <137252219+Asrithya@users.noreply.github.com>
2025-05-20 02:18:17 +0000

    completed

--- src/Module 3/Introduction to ReactJS/Ques_5.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_5.jsx
@@ -3,3 +3,19 @@
 
 // Step 1: Write your code within the file, by the name of component as Profile
 // Step 2: Update App.js to pass props to the Profile component
+
+
+
+import React from 'react';
+
+function Profile(props) {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+}
+
+export default Profile;
