26067409d4e6ea4326302108396eca3689e6d68a
Asrithya Vardhan <137252219+Asrithya@users.noreply.github.com>
2025-05-20 05:51:21 +0000

    completed

--- backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
+++ backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+// Basic route
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+// Handle unknown routes (404)
+app.use((req, res, next) => {
+  res.status(404).send('Route not found');
 });
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Server Error');
+});
+
+module.exports = app;
